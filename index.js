24664162176a26d35e7894ea1285782763cdd896
Braylan Dodson <braylan1005@gmail.com>
2022-05-18 14:54:14 -0400

    commit tree 869ed203de8c709a1de3b1e98b1c4ef86915c632

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = "";
+function upperCaseCustomerName(){
+    const upperCasedName = customerName.toUpperCase();
+    customerName = upperCasedName;
+
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+}
